6c43a18d7fc0295dad773462128794db4a0a0531
Isaac Reynolds <appsforme@outlook.com>
2013-08-18 18:53:54 -0700

    Added SplayTree to sets.

---
+++ DataStructures/DataStructures/Set/SplayTree.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Set
+{
+    public enum Direction {
+	    Left,
+	    Right
+    }
+
+    /**
+     * A splay tree.
+     * 
+     * @author Isaac
+     * @date 10/26/2012
+     */
+    public class SplayTree {
+
+	    private Node overallRoot;
+
+	    /**
+	     * Number of single rotations performed on this tree. Public for testing
+	     * purposes.
+	     */
+	    public long numRotations;
+
+	    public SplayTree() 
+            : this(null) { }
+
+	    private SplayTree(Node root) {
+		    overallRoot = root;
+	    }
+
+	    /**
+	     * Performs a single rotation at node in the given direction (a left
+	     * rotation will move node left and replace node with its right child).
+	     * 
+	     * @param root
+	     * @param direction
+	     */
+	    // Returns a reference to the new root in this tree (what replaced node).
+	    // After this operation, you MUST reassign the parents' subtree to the
+	    // return value.
+	    private Node rotate(Node node, Direction direction) {
+		    Node n = null;
+		    switch (direction) {
+		    case Direction.Right:
+			    n = node.left;
+			    node.left = n.right;
+			    n.right = node;
+			    break;
+		    case Direction.Left:
+			    n = node.right;
+			    node.right = n.left;
+			    n.left = node;
+			    break;
+		    }
+		    numRotations++;
+		    return n;
+	    }
+
+	    /**
+	     * If key is in this, brings it to the root. Otherwise brings to the root a
+	     * key in this that would neighbor key in the dictionary ordering.
+	     * 
+	     * @param key
+	     * @return the new root of this
+	     */
+	    private void splay(int key) {
+		    Tuple ret = splay(key, overallRoot);
+		    overallRoot = ret.rootNode;
+
+		    // If the key was left one down from the root, move it up.
+		    Node r = overallRoot;
+		    if (r == null)
+			    return;
+		    if (r.left != null && r.left.key == ret.splayedKey) {
+			    overallRoot = rotate(overallRoot, Direction.Right);
+		    } else if (r.right != null && r.right.key == ret.splayedKey) {
+			    overallRoot = rotate(overallRoot, Direction.Left);
+		    }
+	    }
+
+	    // Private recursive companion to splay(int)
+	    private Tuple splay(int key, Node current) {
+		    // If you couldn't find key in the tree, return null
+		    if (current == null)
+			    return new Tuple(null, 0);
+
+		    // Recurse down to find key
+		    Tuple ret = null;
+		    if (key == current.key) {
+			    return new Tuple(current, current.key);
+
+		    } else if (key < current.key) {
+			    ret = splay(key, current.left);
+			    current.left = ret.rootNode;
+
+		    } else { // key > current.key
+			    ret = splay(key, current.right);
+			    current.right = ret.rootNode;
+
+		    }
+
+		    // If the returned node is null, then key isn't in this tree, so we
+		    // should splay around the current node instead.
+		    if (ret.rootNode == null)
+			    return new Tuple(current, current.key);
+
+		    // newCurrent will store a reference to the node that will replace
+		    // current in the tree.
+		    Node newCurrent = null;
+
+		    // Perform the appropriate rotation if at depth 2, or return if at
+		    // depth 1.
+		    Node ll = (current.left != null) ? current.left.left : null;
+		    Node lr = (current.left != null) ? current.left.right : null;
+		    Node rr = (current.right != null) ? current.right.right : null;
+		    Node rl = (current.right != null) ? current.right.left : null;
+		    if (ll != null && ll.key == ret.splayedKey) {
+			    // first rotate around current, then rotate around whatever took
+			    // current's place (rotate grandparent of splayedKey, then parent).
+			    newCurrent = rotate(current, Direction.Right);
+			    newCurrent = rotate(newCurrent, Direction.Right);
+
+		    } else if (lr != null && lr.key == ret.splayedKey) {
+			    // first rotate around current's left child, then rotate around
+			    // current.
+			    current.left = rotate(current.left, Direction.Left);
+			    newCurrent = rotate(current, Direction.Right);
+
+		    } else if (rr != null && rr.key == ret.splayedKey) {
+			    newCurrent = rotate(current, Direction.Left);
+			    newCurrent = rotate(newCurrent, Direction.Left);
+
+		    } else if (rl != null && rl.key == ret.splayedKey) {
+			    current.right = rotate(current.right, Direction.Right);
+			    newCurrent = rotate(current, Direction.Left);
+
+		    } else { // the splayedNode is at depth one
+			    newCurrent = current;
+		    }
+
+		    return new Tuple(newCurrent, ret.splayedKey);
+	    }
+	
+	    /**
+	     * Returns true iff key is in this.
+	     * 
+	     */
+	    // Splay the tree around key and then check if key is at the root.
+	    public bool lookup(int key) {
+		    splay(key);
+		    return overallRoot != null && overallRoot.key == key;
+	    }
+
+	    /**
+	     * Inserts the given key into this if it not already in this.
+	     * 
+	     */
+	    // Splay the tree around key and then place key at the root.
+	    public void insert(int key) {
+		    splay(key);
+		    Node newRoot = new Node(key);
+		    if (overallRoot == null) {
+			    overallRoot = newRoot;
+		    } else if (overallRoot.key != key) {
+			    int rootKey = overallRoot.key;
+			    // Replace the root with the new key and move old root's subtrees
+			    // to maintain the search tree property.
+			    if (rootKey < key) {
+				    newRoot.left = overallRoot;
+				    newRoot.right = overallRoot.right;
+				    overallRoot.right = null;
+				    overallRoot = newRoot;
+			    } else if (rootKey > key) {
+				    newRoot.right = overallRoot;
+				    newRoot.left = overallRoot.left;
+				    overallRoot.left = null;
+				    overallRoot = newRoot;
+			    }
+		    }
+	    }
+
+	    /**
+	     * If every key in t1 is less than every key in t2, creates a binary search
+	     * tree containing all keys in t1 and t2, then returns it.
+	     * 
+	     * @return the new SplayTree
+	     */
+	    // Get n2, the root of t2. Splay t1 around n2 so that the root of t1 has
+	    // only a left subtree.
+	    private SplayTree concat(SplayTree t1, SplayTree t2) {
+		    if (t2.overallRoot != null && t1.overallRoot != null) {
+			    t1.splay(t2.overallRoot.key);
+			    t1.overallRoot.right = t2.overallRoot;
+			    return t1;
+		    } else if (t2.overallRoot != null) {
+			    return t2;
+		    } else if (t1.overallRoot != null) {
+			    return t1;
+		    } else { // both empty
+			    return new SplayTree();
+		    }
+	    }
+
+	    /**
+	     * Removes key from this.
+	     * 
+	     */
+	    // Splay the tree around K, then remove root and concatenate its two
+	    // subtrees.
+	    public void delete(int key) {
+		    splay(key);
+		    if (overallRoot != null && overallRoot.key == key) {
+			    SplayTree t1 = new SplayTree(overallRoot.left);
+			    SplayTree t2 = new SplayTree(overallRoot.right);
+			    overallRoot = concat(t1, t2).overallRoot;
+		    }
+	    }
+
+	    /**
+	     * Prints a string representation of this to the console, and returns that
+	     * string.
+	     * 
+	     */
+	    public String display() {
+		    StringBuilder sb = new StringBuilder();
+		    display(overallRoot, 0, sb);
+		    return sb.ToString();
+	    }
+
+	    // Private companion to display()
+	    private void display(Node current, int level, StringBuilder sb) {
+		    String output = "";
+		    for (int i = 0; i < level; i++)
+			    output += "  ";
+		    output += (current == null) ? "-" : current.key.ToString();
+		    output += "\n";
+		    sb.Append(output);
+
+		    if (current != null && (current.left != null || current.right != null)) {
+			    display(current.left, level + 1, sb);
+			    display(current.right, level + 1, sb);
+		    }
+	    }
+	    private class Node {
+		    public Node left;
+		    public Node right;
+		    public readonly int key;
+
+		    public Node(Node left, Node right, int key) {
+			    this.left = left;
+			    this.right = right;
+			    this.key = key;
+		    }
+
+		    public Node(int key) 
+                : this(null, null, key) { }
+	    }
+
+	    private class Tuple {
+		    public readonly Node rootNode;
+		    public readonly int splayedKey;
+
+		    public Tuple(Node parentNode, int splayedKey) {
+			    this.rootNode = parentNode;
+			    this.splayedKey = splayedKey;
+		    }
+	    }
+    }
+}
---
+++ DataStructures/DataStructuresUnitTests/PerformanceTests/SplayTreePerfTest.cs
@@ -0,0 +1,46 @@
+﻿using DataStructures.Set;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresUnitTests.PerformanceTests
+{
+    public class SplayTreeTimeTests
+    {
+        public static void main(String[] args) {
+		    for (int i = 0; i <= 20; i++) {
+			    SplayTree t = new SplayTree();
+			
+			    int n = (int)Math.Pow(2, i);	// number of elements at max size
+			    long m = 3 * n;					// number of operations (insert + lookup)
+			    long T = 0;						// number of single rotations
+			
+			    if (n > Int32.MaxValue)
+				    throw new Exception();
+			
+			    for (int m1 = 0; m1 < n; m1++) 
+				    t.insert(m1);
+			
+			    lookupRand(t, n, m);
+			    lookupRand(t, n, m);
+			
+			    T = t.numRotations;
+			    double tm = (double)T / m ;				// T / m
+			    double ln = Math.Log(n) / Math.Log(2);	// log_2(n)
+			    Console.WriteLine(i + ": ");
+                Console.WriteLine(String.Format("({0}, {1})", ln, tm));
+		    }
+	    }
+
+        private static void lookupRand(SplayTree t, int max, long numOps)
+        {
+            Random r = new Random();
+            for (int i = 0; i < numOps; i++)
+            {
+                t.lookup(r.Next(max));
+            }
+        }
+    }
+}
